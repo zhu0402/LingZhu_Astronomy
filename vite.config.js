ccbf3e8e858a8dc498c83d3863da966f7297e247
li zhu <lzhu83168@gmail.com>
2026-06-15 11:18:09 +1000

    Configure Decap CMS for Netlify Git Gateway

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/REACT2026/',
+  base: process.env.NETLIFY ? '/' : '/REACT2026/',
 })
